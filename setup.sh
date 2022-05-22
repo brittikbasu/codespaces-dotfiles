886bdc848f0a13a215c7b778dba6ecef840d89ea
Brittik Basu <brittikbasu@gmail.com>
2022-05-22 05:30:09 +0530

    add deno install alias

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 export DENO_INSTALL="/home/codespace/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
 alias deno='/home/codespace/.deno/bin/deno'
+alias install-deno='curl -fsSL https://deno.land/install.sh | sh'
